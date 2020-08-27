a68cd1f0b4ee899f2666841abd173449e70f8811
Mark Guthrie <mark.guthrie@abf.com.au>
2020-08-27 16:54:55 +1000

    Added .platform

---
+++ .platform/hooks/prebuild/mount-media.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+yum -y install amazon-efs-utils
+
+# Ugly way to get env variables
+EFS_MOUNT_DIR=`cat /opt/elasticbeanstalk/deployment/env | grep MOUNT_DIRECTORY | tr "=" " " | awk '{print $2}'`
+EFS_FILE_SYSTEM_ID=`cat /opt/elasticbeanstalk/deployment/env | grep FILE_SYSTEM_ID | tr "=" " " | awk '{print $2}'`
+
+echo "Mounting EFS filesystem ${EFS_FILE_SYSTEM_ID} to directory ${EFS_MOUNT_DIR} ..."
+
+echo 'Stopping NFS ID Mapper...'
+service rpcidmapd status &> /dev/null
+if [ $? -ne 0 ] ; then
+    echo 'rpc.idmapd is already stopped!'
+else
+    service rpcidmapd stop
+    if [ $? -ne 0 ] ; then
+        echo 'ERROR: Failed to stop NFS ID Mapper!'
+        exit 1
+    fi
+fi
+
+echo 'Checking if EFS mount directory exists...'
+if [ ! -d ${EFS_MOUNT_DIR} ]; then
+    echo "Creating directory ${EFS_MOUNT_DIR} ..."
+    mkdir -p ${EFS_MOUNT_DIR}
+    if [ $? -ne 0 ]; then
+        echo 'ERROR: Directory creation failed!'
+        exit 1
+    fi
+else
+    echo "Directory ${EFS_MOUNT_DIR} already exists!"
+fi
+
+mountpoint -q ${EFS_MOUNT_DIR}
+if [ $? -ne 0 ]; then
+    echo "mount -t efs -o tls ${EFS_FILE_SYSTEM_ID}:/ ${EFS_MOUNT_DIR}"
+    mount -t efs -o tls ${EFS_FILE_SYSTEM_ID}:/ ${EFS_MOUNT_DIR}
+    if [ $? -ne 0 ] ; then
+        echo 'ERROR: Mount command failed!'
+        exit 1
+    fi
+    chmod 777 ${EFS_MOUNT_DIR}
+    runuser -l  ec2-user -c "touch ${EFS_MOUNT_DIR}/it_works"
+    if [[ $? -ne 0 ]]; then
+        echo 'ERROR: Permission Error!'
+        exit 1
+    else
+        runuser -l  ec2-user -c "rm -f ${EFS_MOUNT_DIR}/it_works"
+    fi
+else
+    echo "Directory ${EFS_MOUNT_DIR} is already a valid mountpoint!"
+fi
+
+echo 'EFS mount complete.'
+
+# The user and group should be wsgi but this doesn't transfer between
+# instances. There is no access to the directories from elsewhere
+# and the data is not critical so oepn to everyone.
+echo 'Setting Permissions'
+chmod 777 /cobalt-media/django-summernote/
+chmod 777 /cobalt-media/health_check_storage_test/
+chmod 777 /cobalt-media/pic_folder/
+
+exit 0
