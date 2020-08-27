0c377e7ec470e9cfe796f135b05179030683c2dc
Mark Guthrie <mark.guthrie@abf.com.au>
2020-08-27 20:23:21 +1000

    install script

--- .platform/hooks/prebuild/install_scripts.sh
+++ .platform/hooks/prebuild/install_scripts.sh
@@ -6,3 +6,22 @@ cat << EOF > /home/ec2-user/.bashrc
 alias x='exit'
 sudo -s
 EOF
+
+cat << EOF > /root/.bashrc
+#!/bin/bash
+# root now so do stuff
+alias x='exit'
+
+# activate virtualenv
+. /var/app/venv/staging-LQM1lest/bin/activate
+
+# set environment variable
+`cat /opt/elasticbeanstalk/deployment/env | awk '{print "export",$1}'`
+
+# change to app directory
+cd /var/app/current
+
+clear
+echo $COBALT_HOSTNAME
+cat /cobalt-media/admin/env.txt
+EOF
